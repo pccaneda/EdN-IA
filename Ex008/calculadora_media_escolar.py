b983f80547c3095fbd8a2be30a7dcaa147c4c13a
pccaneda <cavalcantecaneda@gmail.com>
2025-10-31 00:03:42 -0300

    Atividade 2 - terceiro exercicio

---
+++ Ex008/calculadora_media_escolar.py
@@ -0,0 +1,13 @@
+nota_1 = 7.5
+nota_2 = 8.0
+nota_3 = 6.5
+
+media = (nota_1 + nota_2 + nota_3) / 3
+
+print(
+    f'Nota 1: {nota_1}',
+    f'Nota 2: {nota_2}',
+    f'Nota 3: {nota_3}',
+    f'Média: {media:.2f}',
+    sep='\n'
+)
