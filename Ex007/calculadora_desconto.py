25316dec01e2cec69ece99b6dedba6943bf6664c
pccaneda <cavalcantecaneda@gmail.com>
2025-10-30 23:58:36 -0300

    Atividade 2 - segundo exercicio (formatação)

--- Ex007/calculadora_desconto.py
+++ Ex007/calculadora_desconto.py
@@ -7,8 +7,8 @@ produto_preco_final = produto_preco - valor_descontado
 
 print(
     f'Produto: {produto_nome}', 
-    f'Preço Original: R$ {produto_preco}', 
-    f'Desconto: R$ {valor_descontado}', 
-    f'Preço final: R$ {produto_preco_final}', 
+    f'Preço Original: R$ {produto_preco:.2f}',
+    f'Desconto: R$ {valor_descontado:.2f}',
+    f'Preço final: R$ {produto_preco_final:.2f}',
     sep='\n'
 )
