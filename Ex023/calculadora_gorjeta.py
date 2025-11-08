07ebbeaeeca8d83f45a403aa8048798e8d2fe831
pccaneda <cavalcantecaneda@gmail.com>
2025-11-08 20:26:54 -0300

    Correção: Blocos try/except movidos para dentro do name-main.

--- Ex023/calculadora_gorjeta.py
+++ Ex023/calculadora_gorjeta.py
@@ -3,21 +3,21 @@ def calcula_gorjeta(valor_conta: float, porcentagem_gorjeta: float) -> float:
 
     return gorjeta
 
-try:
-    valor_conta = input('Valor da conta: ')
-    valor_conta = float(valor_conta)
-except ValueError as e:
-    e.add_note('\nO valor da conta deve ser um número.\n')
-    raise
-
-try:
-    porcentagem_gorjeta = input('Porcentagem da gorjeta (valor entre 0 e 1, inclusive): ')
-    porcentagem_gorjeta = float(porcentagem_gorjeta)
-except ValueError as e:
-    e.add_note('\nA porcentagem da gorjeta deve ser um número.\n')
-    raise
-
 if __name__ == '__main__':
+
+    try:
+        valor_conta = input('Valor da conta: ')
+        valor_conta = float(valor_conta)
+    except ValueError as e:
+        e.add_note('\nO valor da conta deve ser um número.\n')
+        raise
+
+    try:
+        porcentagem_gorjeta = input('Porcentagem da gorjeta (valor entre 0 e 1, inclusive): ')
+        porcentagem_gorjeta = float(porcentagem_gorjeta)
+    except ValueError as e:
+        e.add_note('\nA porcentagem da gorjeta deve ser um número.\n')
+        raise
 
     gorjeta = calcula_gorjeta(valor_conta, porcentagem_gorjeta)
     print(f'A gorjeta é: {gorjeta}')
